21b7ad3f85f8068008fac6627f0b035a4fca47d7
Junxian Qi <qijunxian0819@gmail.com>
2021-01-28 23:18:31 +0800

    Configure database and use real tourist route repository

--- JTrip.API/JTrip.API/Startup.cs
+++ JTrip.API/JTrip.API/Startup.cs
@@ -1,7 +1,10 @@
+using JTrip.API.Database;
 using JTrip.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,12 +16,22 @@ namespace JTrip.API
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddTransient<ITouristRouteRepository, MockTouristRouteRepository>();
+            services.AddTransient<ITouristRouteRepository, TouristRouteRepository>();
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseSqlServer(Configuration["DbContext:ConnectionString"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
