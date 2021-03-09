15747c3e333ef14c6e78f19d15fec235a92694ab
Junxian Qi <qijunxian0819@gmail.com>
2021-03-09 11:46:49 +0800

    Create DataSeeding2

--- JTrip.API/JTrip.API/Database/AppDbContext.cs
+++ JTrip.API/JTrip.API/Database/AppDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using JTrip.API.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace JTrip.API.Database
 {
@@ -18,14 +21,15 @@ namespace JTrip.API.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
-            {
-                Id = Guid.NewGuid(),
-                Title = "TestTitle",
-                Description = "TestDescription",
-                OriginalPrice = 0,
-                CreateTime = DateTime.UtcNow
-            });
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePictureJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
